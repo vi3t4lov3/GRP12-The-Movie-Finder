e543f8b4fc969f8272b23735cdf8a1f1a233d452
Tu Nguyen <tunguyen@Tus-MacBookPro.local>
2022-04-11 17:34:19 -0400

    succecced fetch the data form Marvel APIS

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,21 +8,25 @@ var outputPosterEl = document.getElementById('#movie=card');
 var ratingEl = document.getElementById('#rating');
 
 
+
+
+
 var formSubmitHandler = function(event) {
     event.preventDefault();
     var marvelApi = "8dab1b84220523126c00f0c92d4bcb32";
+    var marvelHashKey ='01dcb492e9aeb8c255d6ff032fac2122';
     var omdbApiKey ="77e3425e"
     var giphyApiKey = 'ngancOjMQqKMUDs0pp3lMqtR67sdDMIC'
-    var character = characterEl.value.trim();
+    var character = characterEl.value.replace(/ /g, '%20').trim();
     if (character) {
         if (character == null) {
            characterEl.value = "Please eneter character" 
 
         } else {
-           getOmdbApi(character, omdbApiKey); 
-           getGiphyApi(character, giphyApiKey);
+        //    getOmdbApi(character, omdbApiKey); 
+        //    getGiphyApi(character, giphyApiKey);
         }
-        // getMarvelAPI(character, marvelApi);
+        getMarvelAPI(character, marvelApi, marvelHashKey);
         
     }
 }
@@ -35,30 +39,49 @@ function getGiphyApi(character, giphyApiKey) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data)
-        // for (var i = 0; i < data.length; i++) {
-            
-        // }
-    })
-}
-
-// Marvel API for bio
-function getMarvelAPI(marvelApi) {
-    var requestMarvelUrl = `https://gateway.marvel.com/v1/public/characters?name=character&apikey=${marvelApi}`
-    
-    // console.log(requestMarvelUrl)
-
-    fetch(requestMarvelUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
         // console.log(data)
         // for (var i = 0; i < data.length; i++) {
             
         // }
     })
 }
+
+function getMarvelAPI(character, marvelApi, marvelHashKey) {
+
+    var requestMarvelUrl = `https://gateway.marvel.com/v1/public/characters?name=${character}&ts=1&apikey=${marvelApi}&hash=${marvelHashKey}`
+
+    fetch(requestMarvelUrl)
+    .then(function (response) {
+        console.log(response)
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        for (var i = 0; i < data.data.results.length; i++) {
+            var name = data.data.results[i].name
+           console.log(name) 
+        }
+    })
+}
+
+//list of 20 characters from database
+// function getMarvelAPI(character, marvelApi) {
+
+//     var requestMarvelUrl = `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=8dab1b84220523126c00f0c92d4bcb32&hash=01dcb492e9aeb8c255d6ff032fac2122`
+
+//     fetch(requestMarvelUrl)
+//     .then(function (response) {
+//         // console.log(response)
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data)
+//         for (var i = 0; i < data.data.results.length; i++) {
+//             var name = data.data.results[i].name
+//            console.log(name) 
+//         }
+//     })
+// }
 
 
 // OMDB API for media
@@ -75,7 +98,7 @@ var requestOmdbUrl = `http://www.omdbapi.com/?t=${character}&apikey=77e3425e`;
         return response.json();
     })
     .then(function (data) {
-        console.log(data)
+        // console.log(data)
         var characterName = data.Title;
         var posterImg = data.Poster;
         var ratingList = data.Ratings;
