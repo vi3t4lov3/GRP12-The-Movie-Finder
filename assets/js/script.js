6a748ee655caffa95ac7cadcbaa2453cbdf9d30f
Tu Nguyen <tuinfor@ymail.com>
2022-04-12 02:01:31 -0400

    succeed display the list of movie from searching in ODMB database, & getted the details from data

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,11 @@ var outputEl = document.querySelector('#actors')
 var outputPosterEl = document.getElementById('#movie=card');
 var ratingEl = document.getElementById('#rating');
 
+//golbal variables
+var marvelApi = "8dab1b84220523126c00f0c92d4bcb32";
+var marvelHashKey ='01dcb492e9aeb8c255d6ff032fac2122';
+var omdbApiKey ="77e3425e"
+var giphyApiKey = 'ngancOjMQqKMUDs0pp3lMqtR67sdDMIC'
 //handler the search button
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -20,15 +25,15 @@ var formSubmitHandler = function(event) {
            characterEl.value = "Please eneter character" 
 
         } else {
-           getOmdbApi(character, omdbApiKey); 
-           getGiphyApi(character, giphyApiKey);
-        //    getMarvelAPI(character, marvelApi, marvelHashKey);
+            getMarvelAPI(character);
+            getOmdbApi(character); 
+            getGiphyApi(character);
         }
         
     }
 }
 // call API for GIF, side column
-function getGiphyApi(character, giphyApiKey) {
+function getGiphyApi(character) {
     var requestGiphyApi = `https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=${character}&limit=25&offset=0&rating=g&lang=en`
 
     fetch(requestGiphyApi)
@@ -43,29 +48,25 @@ function getGiphyApi(character, giphyApiKey) {
     })
 }
 
-function getMarvelAPI(character, marvelApi, marvelHashKey) {
+function getMarvelAPI(character) {
 
     var requestMarvelUrl = `https://gateway.marvel.com/v1/public/characters?name=${character}&ts=1&apikey=${marvelApi}&hash=${marvelHashKey}`
 
     fetch(requestMarvelUrl)
     .then(function (response) {
-        // console.log(response)
         return response.json();
     })
     .then(function (marvel) {
-        var marvelCharacterName = marvel.data.results[0].name;
-        var marvelCharacterBio = marvel.data.results[0].events.items[19].name;
-        var marvelCharacterImg = `${marvel.data.results[0].thumbnail.path}.jpg`
+        console.log(marvel);
+        var data = marvel.data.results[0]
+        var marvelCharacterImg = `${data.thumbnail.path}.jpg`
         console.log(marvel)
-        $('#marvel-character-name').append(marvelCharacterName);
+        $('#marvel-character-name').append(data.name);
         $('#marvel-character-img').append(`<img src='${marvelCharacterImg}'>`);
-        $('#marvel-character-bio').append(marvelCharacterBio);
-        // $('#marvel-character-name').append(marvelCharacterName);
-        // $('#marvel-character-name').append(marvelCharacterName);
-        // for (var i = 0; i < data.data.results.length; i++) {
-        //     var name = data.data.results[i].thumbnail.path
-        //    console.log(name) 
-        // }
+        $('#marvel-character-bio').append(data.description);
+        $('#marvel-comics').append(`Comic Available: <a href="${data.comics.collectionURI}" target="_blank"> ${data.comics.available} </a>`);
+        $('#marvel-series').append(`Series Available: ${data.series.available}`);
+        $('#marvel-stories').append(`Storie Available: ${data.stories.available}`);
     })
 }
 
@@ -90,17 +91,16 @@ function getMarvelAPI(character, marvelApi, marvelHashKey) {
 
 
 // OMDB API for media
-function getOmdbApi(character, apikey) {
-var requestOmdbUrl = `https://omdbapi.com/?s=${character}&page=1&apikey=77e3425e`;
+function getOmdbApi(character) {
+var requestOmdbUrl = `https://omdbapi.com/?s=${character}&page=1&apikey=${omdbApiKey}`;
     // console.log(requestOmdbUrl)
     fetch(requestOmdbUrl)
     .then(function (response) {
         return response.json();
     })
-    .then(function (data) {
-        console.log(data)
-        console.log(data.Search);
-        displayMovieList(data.Search); //display the movies on the list
+    .then(function (OMBDdata) {
+        console.log(OMBDdata)
+        displayMovieList(OMBDdata.Search); //display the movies on the list
     //     var characterName = data.Title;
     //     var posterImg = data.Poster;
     //     var ratingList = data.Ratings;
@@ -109,16 +109,47 @@ var requestOmdbUrl = `https://omdbapi.com/?s=${character}&page=1&apikey=77e3425e
     // $('#rating').append(ratingList);
     })
 }
+//display the list of array from the OMDB database
 function displayMovieList(movies) {
     for (var i = 0; i < movies.length; i++) {
         var movieListItem = movies[i].imdbID;
         var moviePoster = movies[i].Poster;
-        console.log(movieListItem);
-        $('#search-list').append(`<div class = 'search-item-thumbnail'> <img src='${moviePoster}' /></div>
+        // console.log(movieListItem);
+        $('#search-list').append(`<div onclick="loadMovieDetail('${movieListItem}')" class = 'search-item-thumbnail'> <img src='${moviePoster}' /></div>
         <div class = 'search-item-info'> <h3>${movies[i].Title}</h3>
         <p>${movies[i].Year}</p>
         `);
     }
 }
-// displayMovieList() 
+
+//fetch the detail movie form the search list & display
+function loadMovieDetail(movieId) {
+    var movieDetailUrl = `http://www.omdbapi.com/?i=${movieId}&apikey=${omdbApiKey}`;
+    fetch(movieDetailUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (details) {
+        console.log(details)
+        $('#search-list-item').append(`
+            <div class = 'movie-poster'>
+                <img src = '${(details.Poster != 'N/A') ? details.Poster : 'image_not_found.png'}' alt = 'movie poster'>
+            </div>
+            <div>
+                <h3 class = 'movie-title'>${details.Title}</h3>
+                <ul class = 'movie-misc-info'>
+                    <li class = 'year'>Year: ${details.Year}</li>
+                    <li class = 'rated'>Ratings: ${details.Rated}</li>
+                    <li class = 'released'>Released: ${details.Released}</li>
+                </ul>
+                <p class = 'genre'><b>Genre:</b> ${details.Genre}</p>
+                <p class = 'writer'><b>Writer:</b> ${details.Writer}</p>
+                <p class = 'actors'><b>Actors: </b>${details.Actors}</p>
+                <p class = 'plot'><b>Plot:</b> ${details.Plot}</p>
+                <p class = 'language'><b>Language:</b> ${details.Language}</p>
+                <p class = 'awards'><b><i class = 'fas fa-award'></i></b> ${details.Awards}</p>
+            </div>
+        `)
+})
+}
 characterSearchEL.addEventListener('submit', formSubmitHandler)
