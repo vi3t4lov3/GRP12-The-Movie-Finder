fad6951bc6e0f8f808c0f02aebbdd9f4ec92e742
Tu Nguyen <tuinfor@ymail.com>
2022-04-14 23:49:17 -0400

    clean up the style & add on the TMBD api

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,11 @@ var outputEl = document.querySelector('#actors')
 var outputPosterEl = document.getElementById('#movie=card');
 var ratingEl = document.getElementById('#rating');
 //golbal variables
-var marvelApi = "8dab1b84220523126c00f0c92d4bcb32";
+var marvelApi = '8dab1b84220523126c00f0c92d4bcb32';
 var marvelHashKey ='01dcb492e9aeb8c255d6ff032fac2122';
-var omdbApiKey ="77e3425e"
-var giphyApiKey = 'ngancOjMQqKMUDs0pp3lMqtR67sdDMIC'
+var omdbApiKey ='77e3425e';
+var giphyApiKey = 'ngancOjMQqKMUDs0pp3lMqtR67sdDMIC';
+var tmdbpiKey = '048b1b9e7d22100a1f7a619469d30c91';
 
 //handler the search button form
 var formSubmitHandler = function(event) {
@@ -24,9 +25,32 @@ var formSubmitHandler = function(event) {
             getMarvelAPI(character);
             getOmdbApi(character); 
             getGiphyApi(character);
+            theMoviedbApi(character) 
         }
         
     }
+}
+
+// call API for GIF, side column
+function theMoviedbApi(character) {
+    var requestTMDBApi = `https://api.themoviedb.org/3/search/person?api_key=${tmdbpiKey}&query=${character}`
+
+    fetch(requestTMDBApi)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (TMDB) {
+        // $('#giphy-gif').empty();
+        console.log(TMDB)
+        // $('#character-bio').empty()
+        $('#character-bio').append(`
+           
+            <div>
+                <h3>Name: ${TMDB.results[0].name}</h3>
+                
+        </div> 
+        `)
+    })
 }
 
 // call API for GIF, side column
@@ -58,43 +82,23 @@ function getMarvelAPI(character) {
     })
     .then(function (marvel) {
         console.log(marvel);
-        var data = marvel.data.results[0]
-        var marvelCharacterImg = `${data.thumbnail.path}.jpg`
-        $('#character-info').empty();
-        $('#character-info').append(`
+        var data = marvel.data.results[0];
+        var marvelCharacterImg = `${data.thumbnail.path}.jpg`;
+        $('#marvel-info').empty()
+        $('#marvel-info').append(`
            <div class="marvel-img">
-                <img src='${marvelCharacterImg}'>;
+                <img src='${marvelCharacterImg}'>
             </div>
             <div>
                 <h3>Name: ${data.name}</h3>
                 <p id="marvel-character-bio">${data.description}</p>
                 <p id="marvel-comics">Comic Available: ${data.comics.available}</p>
-                <p id="marvel-series">${data.series.available}</p>
-                <p id="marvel-stories">${data.stories.available}</p>
+                <p id="marvel-series">Seriesl Available: ${data.series.available}</p>
+                <p id="marvel-stories">Stories Available: ${data.stories.available}</p>
         </div> 
         `)
     })
 }
-
-//list of 20 characters from database
-// function getMarvelAPI(character, marvelApi) {
-
-//     var requestMarvelUrl = `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=8dab1b84220523126c00f0c92d4bcb32&hash=01dcb492e9aeb8c255d6ff032fac2122`
-
-//     fetch(requestMarvelUrl)
-//     .then(function (response) {
-//         // console.log(response)
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data)
-//         for (var i = 0; i < data.data.results.length; i++) {
-//             var name = data.data.results[i].name
-//            console.log(name) 
-//         }
-//     })
-// }
-
 
 // OMDB API for media
 function getOmdbApi(character) {
@@ -107,12 +111,6 @@ var requestOmdbUrl = `https://omdbapi.com/?s=${character}&page=1&apikey=${omdbAp
     .then(function (OMBDdata) {
         // console.log(OMBDdata)
         displayMovieList(OMBDdata.Search); //display the movies on the list
-    //     var characterName = data.Title;
-    //     var posterImg = data.Poster;
-    //     var ratingList = data.Ratings;
-    // $('#actors').append(characterName);
-    // $('#poster').append(`<img src='${posterImg}'>`);
-    // $('#rating').append(ratingList);
     })
 }
 //display the list of array from the OMDB database
